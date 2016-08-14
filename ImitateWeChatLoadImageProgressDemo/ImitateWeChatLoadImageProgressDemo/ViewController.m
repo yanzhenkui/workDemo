d7a3664a6a24aeff9f3b289845e5f27e58e0564c
yanzhenkui <ghsapp@wangxingmandeMacBook-Air.local>
2016-08-14 20:14:15 +0800

    0814修改

--- ImitateWeChatLoadImageProgressDemo/ImitateWeChatLoadImageProgressDemo/ViewController.m
+++ ImitateWeChatLoadImageProgressDemo/ImitateWeChatLoadImageProgressDemo/ViewController.m
@@ -10,17 +10,39 @@
 #import "DALabeledCircularProgressView.h"
 
 @interface ViewController ()
-
+/** 空心 */
 @property (nonatomic, strong) DACircularProgressView *progressView;
-
+/** 是否无线滚动 */
+@property (nonatomic, assign) BOOL isRool;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.progressView = [[DACircularProgressView alloc] initWithFrame:CGRectMake(100.0f, 100.0f, 40.0f, 40.0f)];
+    self.progressView.progress = 0.4f;
+    self.progressView.roundedCorners =YES;
     
-    self.progressView
+    // 无限转动一圈持续时常,一般是在初始化时设置才有效
+    self.progressView.indeterminateDuration = 5.0f;
+
+    
+    [self.view addSubview:_progressView];
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    self.isRool = !self.isRool;
+    if (_isRool) {
+        self.progressView.indeterminate = YES;
+        
+    }else{
+        self.progressView.indeterminate = NO;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
